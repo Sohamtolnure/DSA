74bf28bfb3e613a4a0db42fc2a94927766542526
soham Tolnure <sohamwork9876@gmail.com>
2025-12-29 21:57:35 +0530

    Add bubble sort implementation in C++
    Implemented Bubble Sort algorithm in C++ as part of Data Structures and Algorithms practice.
    The algorithm compares adjacent elements and swaps them until the array is sorted.
    Time Complexity: O(n²) in worst and average cases.
    Space Complexity: O(1).

---
+++ bubble_sort.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//bubble sort
+
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        for(int i = 0;i< nums.size() ;i++){
+            for(int j = 0;j<nums.size() -1-i;j++){
+                if(nums[j]>nums[j+1]){
+                    swap(nums[j],nums[j+1]);
+                }
+            }
+        }
+        return nums;  
+    }
+};
+int main(){
+    }
